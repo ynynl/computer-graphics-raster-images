b15cb393def5a1a490496dac6daca25dcc4525f3
ynynl <yinyin331@gmail.com>
2020-05-09 23:04:55 -0400

    made reflected

--- src/reflect.cpp
+++ src/reflect.cpp
@@ -7,8 +7,19 @@ void reflect(
   const int num_channels,
   std::vector<unsigned char> & reflected)
 {
-  reflected.resize(width*height*num_channels);
+  reflected.reserve(width*height*num_channels);
   ////////////////////////////////////////////////////////////////////////////
   // Add your code here
+    if (!input.empty()) {
+        for (int i = 1; i < input.size()/height; i++) {
+            for (int j = i * width * num_channels; j != (i - 1) * width * num_channels; j -= num_channels) {
+                
+                std::copy(input.begin() + j - num_channels,
+                          input.begin() + j,
+                          std::back_inserter(reflected));
+            }
+        }
+    }
+    
   ////////////////////////////////////////////////////////////////////////////
 }
--- src/write_ppm.cpp
+++ src/write_ppm.cpp
@@ -3,6 +3,11 @@
 #include <cassert>
 #include <iostream>
 
+#include <sstream>
+#include <string>
+
+
+
 bool write_ppm(
   const std::string & filename,
   const std::vector<unsigned char> & data,
@@ -15,6 +20,24 @@ bool write_ppm(
     ".ppm only supports RGB or grayscale images");
   ////////////////////////////////////////////////////////////////////////////
   // Replace with your code here:
+    std::ofstream outfile (filename);
+    std::ostringstream vts;
+
+    if (!data.empty()) {
+        std::copy(data.begin(),
+                  data.end(),
+                  std::ostream_iterator<int>(vts, " "));
+    }
+
+    outfile <<
+                "P" + std::to_string(num_channels) + "\n"
+                + std::to_string(width) + " " + std::to_string(height) + "\n"
+                + std::to_string(255) + "\n"
+                + vts.str()
+    << std::endl;
+
+    outfile.close();
+    
   return false;
   ////////////////////////////////////////////////////////////////////////////
 }
