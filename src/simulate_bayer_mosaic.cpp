a1b297cd352a9e31e1ed74c1b01c0c8fa018440a
ynynl <yinyin331@gmail.com>
2020-05-10 16:18:44 -0400

    made bayer mosaic

--- src/simulate_bayer_mosaic.cpp
+++ src/simulate_bayer_mosaic.cpp
@@ -6,8 +6,27 @@ void simulate_bayer_mosaic(
   const int & height,
   std::vector<unsigned char> & bayer)
 {
-  bayer.resize(width*height);
+  bayer.reserve(width*height);
   ////////////////////////////////////////////////////////////////////////////
   // Add your code here
+    int row, col;
+    for (int i = 0; i < rgb.size() ; i += 3) {
+        row = i / (3 * width);
+        col = (i / 3) % (width);
+        
+        if (row % 2 == 0) {
+            if (col % 2 == 0) {
+                bayer.push_back(rgb.at(i + 1)); //g
+            } else {
+                bayer.push_back(rgb.at(i + 2)); //b
+            }
+        } else {
+            if (col % 2 == 0) {
+                bayer.push_back(rgb.at(i)); //r
+            } else {
+                bayer.push_back(rgb.at(i + 1)); //g
+            }
+        }
+    }
   ////////////////////////////////////////////////////////////////////////////
 }
