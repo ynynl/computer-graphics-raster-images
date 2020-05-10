d936a71911d25a751a3d423ecc3fa6ff6c57e5b3
ynynl <yinyin331@gmail.com>
2020-05-10 00:09:28 -0400

    made rotated.cpp

--- src/rotate.cpp
+++ src/rotate.cpp
@@ -7,8 +7,18 @@ void rotate(
   const int num_channels,
   std::vector<unsigned char> & rotated)
 {
-  rotated.resize(height*width*num_channels);
+  rotated.reserve(height*width*num_channels);
   ////////////////////////////////////////////////////////////////////////////
   // Add your code here
+    int n_width = width * num_channels;
+    
+    for (int i = n_width; i > 0; i -= num_channels) {
+        for (int j = i; j != (height) * n_width + i; j += n_width) {
+            
+            std::copy(input.begin() + j - num_channels,
+                      input.begin() + j,
+                      std::back_inserter(rotated));
+        }
+    }
   ////////////////////////////////////////////////////////////////////////////
 }
